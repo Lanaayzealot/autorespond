d76ab91a6ab45eddc07b9f4a01b843b4bc348e26
Lanaayzealot <Lanalbsafety@gmail.com>
2025-03-05 20:37:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def main():
     print("✅ Bot is running with webhook...")
 
     # Start Flask server
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
 if __name__ == "__main__":
     main()
